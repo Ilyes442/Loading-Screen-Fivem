86c08a7deb7884326c10d6388a88f84e01f0d6be
Ilyes442 <119808847+Ilyes442@users.noreply.github.com>
2024-09-26 16:11:08 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author "Ilyes4"
+version "1.0"
+client_script 'client.lua'
+
+loadscreen_cursor 'yes'
+loadscreen 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/assets/**/*.*',
+    'html/assets/**/**/*.*'
+}
